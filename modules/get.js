e967695d4c41ebcbee9c6f218bcebdc423ff0c0e
Mentor <mentor@palokaj.co>
2017-03-10 13:56:18 +0100

    syntax fix

--- modules/get.js
+++ modules/get.js
@@ -5,11 +5,11 @@ const get = url => {
 		request( { url: url, headers: { 'User-Agent': 'request' } }, ( err, response, body ) => {
 			if ( process.env.debug || process.env.verbose ) {
 				let color = response ? ( response.statusCode == 200 ? colors.green : colors.red ) : colors.red
-				console.log( color ( ( response ? response.statusCode : 000 ) + ': ' + url ) )
+				console.log( color ( ( response ? response.statusCode : '000' ) + ': ' + url ) )
 			}
 			if ( err ) return reject( { url: url, err: err } )
-			if ( !response ) return reject( { url: url, err: response.statusCode || 000 } )
-			response.statusCode == 200 ? resolve( { url: url, html: body } ) : reject( { url: url, err: response.statusCode || 000 } )
+			if ( !response ) return reject( { url: url, err: response.statusCode || '000' } )
+			response.statusCode == 200 ? resolve( { url: url, html: body } ) : reject( { url: url, err: response.statusCode || '000' } )
 		} )	
 	} )
 }
